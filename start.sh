5998c3d6c96e3a348a225de81dde5a89bf8034e3
pepperbuger <kjs0470@gmail.com>
2025-03-02 19:10:22 +0900

    re change

--- start.sh
+++ start.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-
-echo "🔧 Installing ODBC dependencies..."
+echo "Installing ODBC dependencies..."
 apt-get update && apt-get install -y unixodbc unixodbc-dev
 
-# ODBC 드라이버가 제대로 설치되었는지 확인
-echo "🔍 Checking installed ODBC drivers..."
-odbcinst -q -d
-
-echo "🚀 Starting Gunicorn server..."
-gunicorn -w 1 -b 0.0.0.0:8080 app:app --timeout 120 --log-level=debug
+echo "Starting the Gunicorn server..."
+gunicorn -w 2 -b 0.0.0.0:8080 app:app --timeout 120 --log-level=debug
