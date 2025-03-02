1573f71b61dff7a8d4d2dcfa20bcd57f55b07be9
pepperbuger <kjs0470@gmail.com>
2025-03-02 20:25:40 +0900

    re change

--- start.sh
+++ start.sh
@@ -4,4 +4,5 @@ apt-get update;
 apt-get install -y unixodbc unixodbc-dev;
 
 echo "Starting the Gunicorn server..."
-exec gunicorn -w 2 -b 0.0.0.0:8080 app:app --timeout 120 --log-level=debug
+PORT=${PORT:-8080}
+exec gunicorn -w 2 -b 0.0.0.0:$PORT app:app --timeout 120 --log-level=debug
